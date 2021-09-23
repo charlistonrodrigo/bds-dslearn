3a3a22ec6adaff159000b6a3cbbcc5af5d083948
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-09-23 20:37:38 -0300

    Backend project created

---
+++ backend/src/test/java/com/charlistonrodrigo/dslearnbds/DslearnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlistonrodrigo.dslearnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
